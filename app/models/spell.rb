588cdcf6aa763dbbe0dba96a7d07acc7b7dcdda5
Kate <itsyehudit@gmail.com>
2019-11-19 23:49:14 +0100

    Add validations for spells

--- app/models/spell.rb
+++ app/models/spell.rb
@@ -1,2 +1,10 @@
 class Spell < ApplicationRecord
+  validates :name, presence: true
+  validates :level, numericality: {greater_than_or_equal_to: 0}
+  validates :range, presence: true
+  validates :school, presence: true
+  validates :casting_time, presence: true
+  validates :duration, presence: true
+  validates :description_short, presence: true
+  validates :description_long, presence: true
 end

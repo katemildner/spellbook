74789c49a663a5bb85c537dd44795ed6cd4c9455
Kate <itsyehudit@gmail.com>
2019-11-24 16:52:44 +0100

    Remove user_list

--- app/models/user_list.rb
+++
@@ -1,2 +0,0 @@
-class UserList < ApplicationRecord
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_23_215104) do
+ActiveRecord::Schema.define(version: 2019_11_24_153742) do
 
   create_table "spell_lists", force: :cascade do |t|
     t.integer "user_list_id"
@@ -30,10 +30,6 @@ ActiveRecord::Schema.define(version: 2019_10_23_215104) do
     t.text "description_long"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-  end
-
-  create_table "user_lists", force: :cascade do |t|
-    t.string "list_title"
   end
 
 end

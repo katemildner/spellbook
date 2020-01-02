1174b33da169d843bb02ff93addb63df77cdfd12
Katarzyna Mildner <itsyehudit@gmail.com>
2020-01-02 23:45:18 +0100

    Remove redundant routes for user_lists

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   resources :spell_lists
-  resources :user_lists
   resources :spells
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

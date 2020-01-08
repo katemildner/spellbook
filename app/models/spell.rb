7bfce6bbc28ccecd38404164b55538b6aab2eb90
Katarzyna Mildner <itsyehudit@gmail.com>
2020-01-08 22:44:28 +0100

    Create scopes in spell model and using in spell_lists/_form for ordering spells by level and name and grouping by level

--- app/models/spell.rb
+++ app/models/spell.rb
@@ -13,4 +13,16 @@ class Spell < ApplicationRecord
   def to_label
     "#{name} | #{level} | #{description_short}"
   end
+
+  scope :by_level_and_name, -> { order(:level, :name) }
+  scope :level_zero, -> { where ("level = 0") }
+  scope :level_one, -> { where ("level = 1") }
+  scope :level_two, -> { where ("level = 2") }
+  scope :level_three, -> { where ("level = 3") }
+  scope :level_four, -> { where ("level = 4") }
+  scope :level_five, -> { where ("level = 5") }
+  scope :level_six, -> { where ("level = 6") }
+  scope :level_seven, -> { where ("level = 7") }
+  scope :level_eight, -> { where ("level = 8") }
+  scope :level_nine, -> { where ("level = 9") }
 end

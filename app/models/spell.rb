c29558a259949643bb1dc48ff9f700e4b1a4618a
Katarzyna Mildner <itsyehudit@gmail.com>
2020-01-08 22:35:48 +0100

    Change label_method name from to_simple_form_multiple_labels to simple_form specific: to_label

--- app/models/spell.rb
+++ app/models/spell.rb
@@ -10,7 +10,7 @@ class Spell < ApplicationRecord
 
   has_and_belongs_to_many :spell_lists
 
-  def to_simple_form_multiple_labels
+  def to_label
     "#{name} | #{level} | #{description_short}"
   end
 end
